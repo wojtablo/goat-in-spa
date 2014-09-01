347296d0403711899aeb62b1a85e31f103302ddd
dabldzordz <dabldzordz@gmail.com>
2014-09-02 01:11:40 +0200

    dodany bower_components + scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
+$(document).ready(function() {
+    // Scroll the whole document
+    $('#section-link').localScroll({
+        target:'body'
+    });
+
+});
 $(document).foundation();
