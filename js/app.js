ed86a247096775145a148f1f11708aa49b376db7
Wojciech Mikołajewski <wojciechmikolajewski@gmail.com>
2014-09-03 22:01:45 +0200

    Slider upgrade

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($) {
     $('#content-slider-1').royalSlider({
         autoHeight: true,
         arrowsNav: true,
-        fadeinLoadedSlide: true,
+        fadeinLoadedSlide: false,
         arrowsNavAutoHide: true,
         controlNavigationSpacing: 0,
         controlNavigation: 'bullets',
@@ -18,6 +18,37 @@ jQuery(document).ready(function($) {
         loopRewind: true,
         numImagesToPreload: 6,
         keyboardNavEnabled: true,
-        usePreloader: false
+        usePreloader: false,
+        navigateByClick: false,
+        transitionSpeed:300
+    });
+});
+
+$(window).load( function(){
+    var $imgs = $("#globalContainer");
+
+    $imgs.each( function(){
+        var cW = $(this).parent().width();
+        var cH = $(this).parent().height();
+        var w = $(this)[0].clientWidth; //I want the CURRENT width, not original!!
+        var h = $(this)[0].clientHeight; //I want the CURRENT height, not original!!
+        var dW = w - cW;
+        var dH = h - cH;
+
+        console.log( cW + " " + cH + " " + w + " " + h + " " + dW + " " + dH );
+
+        if ( w > h ){
+            $(this).addClass("wide");
+            $(this).css("left",  -dW/2);
+        }
+        else if ( w < h ){
+            $(this).addClass("tall");
+            $(this).css("top",  -dH/2);
+        }
+        else { $(this).addClass("square"); }
+        var w = $(this).width(); //I want the CURRENT width, not original!!
+        var h = $(this).height(); //I want the CURRENT height, not original!!
+        console.log( w + " " + h );
+
     });
 });
