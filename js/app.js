97349ba059425ef9a88146c5218f370e8edb7bf2
Wojciech Mikołajewski <wojciechmikolajewski@gmail.com>
2014-09-03 15:42:28 +0200

    Add Slider plugin

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-//$(document).ready(function() {
-//    // Scroll the whole document
-//    $('#section-link').localScroll({
-//        target:'body'
-//    });
-//
-//});
 $(document).foundation();
+
+jQuery(document).ready(function($) {
+    // Please note that autoHeight option has some conflicts with options like imageScaleMode, imageAlignCenter and autoScaleSlider
+    // it's recommended to disable them when using autoHeight module
+    $('#content-slider-1').royalSlider({
+        autoHeight: true,
+        arrowsNav: true,
+        fadeinLoadedSlide: true,
+        arrowsNavAutoHide: true,
+        controlNavigationSpacing: 0,
+        controlNavigation: 'bullets',
+        imageScaleMode: 'none',
+        imageAlignCenter:false,
+        loop: true,
+        loopRewind: true,
+        numImagesToPreload: 6,
+        keyboardNavEnabled: true,
+        usePreloader: false
+    });
+});
