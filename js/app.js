53abba91d8f399a9b5bb56ea42e6d4e0b0fe63df
Wojciech Mikołajewski <wojciechmikolajewski@gmail.com>
2014-09-04 03:14:27 +0200

    Conditional slider configuration in media query

--- js/app.js
+++ js/app.js
@@ -5,6 +5,19 @@ $(document).foundation();
 jQuery(document).ready(function($) {
     // Please note that autoHeight option has some conflicts with options like imageScaleMode, imageAlignCenter and autoScaleSlider
     // it's recommended to disable them when using autoHeight module
+
+    // Using Modernizr and media queries to deactivate Royal Slider on smartphones
+//    if (matchMedia('only screen and (min-device-width : 20px) and (max-device-width : 720px)').matches) {
+//        // remove class
+//        $('#content-slider-2').attr('id', 'NoGallery');
+//        $('#NoGallery').removeClass('royalSlider');
+//    }
+//    else {
+//        // add class
+//        $('#NoGallery').attr('id', 'slider');
+//        $('#content-slider-2').addClass('royalSlider');
+//    }
+
     $('#content-slider-1').royalSlider({
         autoHeight: true,
         arrowsNav: true,
@@ -22,33 +35,34 @@ jQuery(document).ready(function($) {
         navigateByClick: false,
         transitionSpeed:300
     });
+    if(Modernizr.mq('only all and (max-width: 720px)')){
+        $('#content-slider-2').royalSlider({
+            autoHeight: true,
+            arrowsNav: true,
+            fadeinLoadedSlide: false,
+            arrowsNavAutoHide: true,
+            controlNavigationSpacing: 0,
+            controlNavigation: 'bullets',
+            imageScaleMode: 'none',
+            imageAlignCenter:false,
+            loop: true,
+            loopRewind: true,
+            numImagesToPreload: 6,
+            keyboardNavEnabled: true,
+            usePreloader: false,
+            navigateByClick: false,
+            transitionSpeed:300
+        });
+    }
+
 });
 
-$(window).load( function(){
-    var $imgs = $("#globalContainer");
-
-    $imgs.each( function(){
-        var cW = $(this).parent().width();
-        var cH = $(this).parent().height();
-        var w = $(this)[0].clientWidth; //I want the CURRENT width, not original!!
-        var h = $(this)[0].clientHeight; //I want the CURRENT height, not original!!
-        var dW = w - cW;
-        var dH = h - cH;
-
-        console.log( cW + " " + cH + " " + w + " " + h + " " + dW + " " + dH );
-
-        if ( w > h ){
-            $(this).addClass("wide");
-            $(this).css("left",  -dW/2);
-        }
-        else if ( w < h ){
-            $(this).addClass("tall");
-            $(this).css("top",  -dH/2);
-        }
-        else { $(this).addClass("square"); }
-        var w = $(this).width(); //I want the CURRENT width, not original!!
-        var h = $(this).height(); //I want the CURRENT height, not original!!
-        console.log( w + " " + h );
-
-    });
-});
+//$(window).load( function(){
+//    Foundation.utils.image_loaded($('img#logo__footer'), function(){
+//        console.log('Image Loaded! :)');
+//    });
+//    if(Modernizr.mq('only all and (min-width: 720px)')){
+//        $('#content-slider-2').removeAttr("id");
+//        console.log("Remover Slider ID from section#10");
+//    }
+//});
