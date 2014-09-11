627f6c43f9dff6da91b493f761cbe2403bd06a0d
Wojciech Mikołajewski <wojciechmikolajewski@gmail.com>
2014-09-11 23:08:15 +0200

    News section update. Added custom reveal look and section fixes.

--- js/app.js
+++ js/app.js
@@ -90,8 +90,26 @@ requirejs([ "jquery.min",
          * Include Foundation settings
          */
         $(document).foundation({
-            reveal : {
-                animation_speed: 500
+            reveal: {
+                animation: 'fadeAndPop',
+                animation_speed: 15,
+                close_on_background_click: false,
+                dismiss_modal_class: 'close-reveal-modal',
+                bg_class: 'reveal-modal-bg',
+                root_element: '#slider-news',
+                bg: $('.reveal-modal-bg'),
+                css: {
+                    open: {
+                        'opacity': 0,
+                        'visibility': 'visible',
+                        'display': 'block'
+                    },
+                    close: {
+                        'opacity': 1,
+                        'visibility': 'hidden',
+                        'display': 'none'
+                    }
+                }
             },
             equalizer : {
                 // Specify if Equalizer should make elements equal height once they become stacked.
