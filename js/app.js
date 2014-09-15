ee151192ae58257c8e08666872595c9efba861b9
Wojciech Mikołajewski <wojciechmikolajewski@gmail.com>
2014-09-15 05:37:20 +0200

    Patch v.3.5.5 - Integration of jQuery.ScrollTo for top navigation

--- js/app.js
+++ js/app.js
@@ -77,21 +77,86 @@ requirejs([ "jquery.min",
         var nanobar = new Nanobar(options);
         nanobar.go(50);
 
+
+        /***************************************************************************************************************
+         Scroll
+         ***************************************************************************************************************/
+        $.scrollTo = $.fn.scrollTo = function(x, y, options){
+            if (!(this instanceof $)) return $.fn.scrollTo.apply($('html, body'), arguments);
+
+            options = $.extend({}, {
+                gap: {
+                    x: 0,
+                    y: 0
+                },
+                animation: {
+                    easing: 'swing',
+                    duration: 600,
+                    complete: $.noop,
+                    step: $.noop
+                }
+            }, options);
+
+            return this.each(function(){
+                var elem = $(this);
+                elem.stop().animate({
+                    scrollLeft: !isNaN(Number(x)) ? x : $(y).offset().left + options.gap.x,
+                    scrollTop: !isNaN(Number(y)) ? y : $(y).offset().top + options.gap.y
+                }, options.animation);
+            });
+        };
+
+
         /**
-         * Web font loader
+         * This part causes smooth scrolling using scrollto.js
+         * We target all a tags inside the nav, and apply the scrollto.js to it.
          */
-//        WebFont.load({
-//            custom: {
-//                families: ['MyriadPro-LightCond','MyriadPro-SemiboldCond,OpenSans-Bold,OpenSans-Light,OpenSans-Regular'],
-//                urls: ['/stylesheets/app.css']
-//            },
-//            timeout: 5000
-//
-//        });
+        $(".top-bar-section a").click(function(evn){
+            evn.preventDefault();
+            $('html,body').scrollTo(this.hash, this.hash);
+        });
+
+
+
         /**
-         * Royal slider
+         * This part handles the highlighting functionality.
+         * We use the scroll functionality again, some array creation and
+         * manipulation, class adding and class removing, and conditional testing
          */
-        //$(".rsMainSlideImage").css("margin-top",0);
+        var aChildren = $(".top-bar-section ul.right li").children(); // find the a children of the list items
+        var aArray = []; // create the empty aArray
+        for (var i=0; i < aChildren.length; i++) {
+            var aChild = aChildren[i];
+            var ahref = $(aChild).attr('href');
+            aArray.push(ahref);
+        } // this for loop fills the aArray with attribute href values
+        console.log(aArray);
+
+        $(window).scroll(function(){
+            var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+            var windowHeight = $(window).height(); // get the height of the window
+            var docHeight = $(document).height();
+
+            for (var i=0; i < aArray.length; i++) {
+                var theID = aArray[i];
+                var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+                var divHeight = $(theID).height(); // get the height of the div in question
+                if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                    $("a[href='" + theID + "']").parent().addClass("active");
+                } else {
+                    $("a[href='" + theID + "']").parent().removeClass("active");
+                }
+            }
+
+            if(windowPos + windowHeight == docHeight) {
+                if (!$("nav li:last-child a").hasClass("nav-active")) {
+                    var navActiveCurrent = $(".nav-active").attr("href");
+                    $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                    $("nav li:last-child a").addClass("nav-active");
+                }
+            }
+
+        });
 
 
 
@@ -158,7 +223,7 @@ requirejs([ "jquery.min",
             });
         });
 
-        /*************************************************************************************
+        /***************************************************************************************************************
          * SLIDERS
          *
          * @type {*|jQuery|HTMLElement}
@@ -175,10 +240,6 @@ requirejs([ "jquery.min",
         var sliderStatut = $('#slider-statut');
         sliderStatut.prepend(sliderStatut.find('.rsArrow'));
         nanobar.go(100);
-
-        window.onload = function(){
-            //alert("stop, teraz wegorz");
-        }
 
 
     })($, window);
