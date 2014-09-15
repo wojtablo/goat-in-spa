83a93cf31b446dba664e61f9d0d65841b9aec087
Wojciech Mikołajewski <wojciechmikolajewski@gmail.com>
2014-09-16 01:21:33 +0200

    Patch 3.6. Pixel perfect correction

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
  * Require config
  */
 require.config({
+    //baseUrl: 'fileadmin/templates/fundacjakozminski/_tmpl/_pages/js/',
     baseUrl: 'js/',
     paths: {
         'jquery': 'jquery.min',
@@ -68,6 +69,7 @@ requirejs([ "jquery.min",
 
         var $ =jQuery.noConflict();
 
+
         var $doc = $(document),
             Modernizr = window.Modernizr;
 
@@ -123,40 +125,42 @@ requirejs([ "jquery.min",
          * We use the scroll functionality again, some array creation and
          * manipulation, class adding and class removing, and conditional testing
          */
-        var aChildren = $(".top-bar-section ul.right li").children(); // find the a children of the list items
-        var aArray = []; // create the empty aArray
-        for (var i=0; i < aChildren.length; i++) {
-            var aChild = aChildren[i];
-            var ahref = $(aChild).attr('href');
-            aArray.push(ahref);
-        } // this for loop fills the aArray with attribute href values
-        console.log(aArray);
-
-        $(window).scroll(function(){
-            var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-            var windowHeight = $(window).height(); // get the height of the window
-            var docHeight = $(document).height();
-
-            for (var i=0; i < aArray.length; i++) {
-                var theID = aArray[i];
-                var divPos = $(theID).offset().top; // get the offset of the div from the top of page
-                var divHeight = $(theID).height(); // get the height of the div in question
-                if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
-                    $("a[href='" + theID + "']").parent().addClass("active");
-                } else {
-                    $("a[href='" + theID + "']").parent().removeClass("active");
-                }
-            }
-
-            if(windowPos + windowHeight == docHeight) {
-                if (!$("nav li:last-child a").hasClass("nav-active")) {
-                    var navActiveCurrent = $(".nav-active").attr("href");
-                    $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
-                    $("nav li:last-child a").addClass("nav-active");
-                }
-            }
-
-        });
+        //var aChildren = $(".top-bar-section ul.right li").children(); // find the a children of the list items
+        //var aArray = []; // create the empty aArray
+        //for (var i=0; i < aChildren.length; i++) {
+        //    var aChild = aChildren[i];
+        //    var ahref = $(aChild).attr('href');
+        //    //var ahrefClean = ahref.split('#')
+        //    //aArray.push(ahrefClean[1]);
+        //    aArray.push(ahref);
+        //} // this for loop fills the aArray with attribute href values
+        //console.log(aArray);
+        //
+        //$(window).scroll(function(){
+        //    var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        //    var windowHeight = $(window).height(); // get the height of the window
+        //    var docHeight = $(document).height();
+        //
+        //    for (var i=0; i < aArray.length; i++) {
+        //        var theID = aArray[i];
+        //        var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+        //        var divHeight = $(theID).height(); // get the height of the div in question
+        //        if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+        //            $("a[href='" + theID + "']").parent().addClass("active");
+        //        } else {
+        //            $("a[href='" + theID + "']").parent().removeClass("active");
+        //        }
+        //    }
+        //
+        //    if(windowPos + windowHeight == docHeight) {
+        //        if (!$("nav li:last-child a").hasClass("nav-active")) {
+        //            var navActiveCurrent = $(".nav-active").attr("href");
+        //            $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+        //            $("nav li:last-child a").addClass("nav-active");
+        //        }
+        //    }
+        //
+        //});
 
 
 
@@ -223,6 +227,8 @@ requirejs([ "jquery.min",
             });
         });
 
+
+        //------------------------------------------------------
         /***************************************************************************************************************
          * SLIDERS
          *
